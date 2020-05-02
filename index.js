61f559fa3526ac5ab7a1c1ce0428ff1027a2646a
islamMaruf <marufislam7424@gmail.com>
2020-05-03 02:27:32 +0600

    implement reducers

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 
 const BUY_CAKE = 'BUY_CAKE';
 
-function buyCake(){
+function buyCake() {
     return {
-        type : BUY_CAKE,
-        info : 'first redux action'
+        type: BUY_CAKE,
+        info: 'first redux action'
     }
 }
+
+const initialState = {
+    numberOfCakes: 10
+}
+
+const reducers = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE :
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes - 1
+            }
+        default:
+            return state;
+    }
+};
