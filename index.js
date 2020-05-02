81c0b1288a7579b0bfca96c5eb9cea33885cf07f
islamMaruf <marufislam7424@gmail.com>
2020-05-03 02:56:06 +0600

    implement store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 //actions
 
 const BUY_CAKE = 'BUY_CAKE';
@@ -13,6 +16,7 @@ const initialState = {
     numberOfCakes: 10
 }
 
+//reducers
 const reducers = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE :
@@ -24,3 +28,11 @@ const reducers = (state = initialState, action) => {
             return state;
     }
 };
+
+const store = createStore(reducers);
+console.log('initial state '+store.getState() );
+const unsubscribe = store.subscribe(() => console.log('updated state ' + store.getState()))
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
